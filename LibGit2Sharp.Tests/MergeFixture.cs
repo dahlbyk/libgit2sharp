4a418442f3c77c47aea3409e77eae46319bf0be4
Edward Thomson <ethomson@microsoft.com>
2013-01-09 16:51:59 +0100

    Introduce Repository.MergeHead

--- LibGit2Sharp.Tests/MergeFixture.cs
+++ LibGit2Sharp.Tests/MergeFixture.cs
@@ -1,4 +1,6 @@
-﻿using LibGit2Sharp.Tests.TestHelpers;
+﻿using System.IO;
+using System.Linq;
+using LibGit2Sharp.Tests.TestHelpers;
 using Xunit;
 
 namespace LibGit2Sharp.Tests
@@ -12,6 +14,7 @@ namespace LibGit2Sharp.Tests
             using (var repo = Repository.Init(scd.DirectoryPath))
             {
                 Assert.Equal(true, repo.Index.IsFullyMerged);
+                Assert.Empty(repo.MergeHead);
             }
         }
 
@@ -21,11 +24,34 @@ namespace LibGit2Sharp.Tests
             using (var repo = new Repository(StandardTestRepoWorkingDirPath))
             {
                 Assert.Equal(true, repo.Index.IsFullyMerged);
+                Assert.Empty(repo.MergeHead);
 
                 foreach (var entry in repo.Index)
                 {
                     Assert.Equal(StageLevel.Staged, entry.StageLevel);
                 }
+            }
+        }
+
+        [Fact]
+        public void CanRetrieveTheBranchBeingMerged()
+        {
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoPath);
+            using (var repo = new Repository(path.RepositoryPath))
+            {
+                const string firstBranch = "9fd738e8f7967c078dceed8190330fc8648ee56a";
+                const string secondBranch = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
+
+                string mergeHeadPath = Path.Combine(repo.Info.Path, "MERGE_HEAD");
+                File.WriteAllText(mergeHeadPath, firstBranch + "\n" + secondBranch + "\n");
+
+                Assert.Equal(CurrentOperation.Merge, repo.Info.CurrentOperation);
+
+                MergeHead[] mergedHeads = repo.MergeHead.ToArray();
+                Assert.Equal("MERGE_HEAD[0]", mergedHeads[0].Name);
+                Assert.Equal(firstBranch, mergedHeads[0].Tip.Id.Sha);
+                Assert.Equal("MERGE_HEAD[1]", mergedHeads[1].Name);
+                Assert.Null(mergedHeads[1].Tip);
             }
         }
     }
---
+++ LibGit2Sharp/MergeHeadCollection.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LibGit2Sharp.Core;
+
+namespace LibGit2Sharp
+{
+    /// <summary>
+    /// The commit IDs that are currently being merged into a repository.
+    /// </summary>
+    public class MergeHeadCollection : IEnumerable<MergeHead>
+    {
+        internal readonly Repository repo;
+
+        /// <summary>
+        ///   Needed for mocking purposes.
+        /// </summary>
+        protected MergeHeadCollection()
+        { }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "MergeHeadCollection" /> class.
+        /// </summary>
+        /// <param name = "repo">The repo.</param>
+        internal MergeHeadCollection(Repository repo)
+        {
+            this.repo = repo;
+        }
+
+        #region IEnumerable<Commit> Members
+
+        /// <summary>
+        ///   Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An <see cref = "IEnumerator{T}" /> object that can be used to iterate through the collection.</returns>
+        public virtual IEnumerator<MergeHead> GetEnumerator()
+        {
+            int i = 0;
+            return Proxy.git_repository_mergehead_foreach(repo.Handle,
+                commitId => new MergeHead(repo, new ObjectId(commitId), i++))
+                .GetEnumerator();
+        }
+
+        /// <summary>
+        ///   Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An <see cref = "IEnumerator" /> object that can be used to iterate through the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
