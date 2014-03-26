da0aa4a160ab6702ce0ef1b2d429ba78ccf3bea5
Keith Dahlby <dahlbyk@gmail.com>
2014-03-25 23:11:01 -0500

    squash! Introduce default credentials
    Add XML docs

--- LibGit2Sharp/DefaultCredentials.cs
+++ LibGit2Sharp/DefaultCredentials.cs
@@ -9,6 +9,15 @@ namespace LibGit2Sharp
     /// </summary>
     public sealed class DefaultCredentials : Credentials
     {
+        /// <summary>
+        /// Callback to acquire a credential object.
+        /// </summary>
+        /// <param name="cred">The newly created credential object.</param>
+        /// <param name="url">The resource for which we are demanding a credential.</param>
+        /// <param name="usernameFromUrl">The username that was embedded in a "user@host"</param>
+        /// <param name="types">A bitmask stating which cred types are OK to return.</param>
+        /// <param name="payload">The payload provided when specifying this callback.</param>
+        /// <returns>0 for success, &lt; 0 to indicate an error, &gt; 0 to indicate no credential was acquired.</returns>
         protected internal override int GitCredentialHandler(out IntPtr cred, IntPtr url, IntPtr usernameFromUrl, uint types, IntPtr payload)
         {
             return NativeMethods.git_cred_default_new(out cred);
